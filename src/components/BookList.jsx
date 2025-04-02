f0c73a0d614b9aa276b1c4e4b2d3084ef4b4963b
davidelong0 <davidelong0@hotmail.com>
2025-04-02 17:34:18 +0200

    eliminato card dentro card

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,19 +1,13 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
-const BookList = ({ books }) => {  
-  if (!books || !Array.isArray(books)) {
-    return <p>No books available</p>;
-  }
-
+const BookList = ({ books }) => {
   return (
     <Container className="mt-5">
       <Row className="g-4">
         {books.map((book) => (
           <Col key={book.asin} md={4} lg={3} sm={6}>
-            <Card className="shadow-lg border-0 rounded-4 h-100 d-flex flex-column bg-dark text-light">
-              <SingleBook book={book} />
-            </Card>
+            <SingleBook book={book} />
           </Col>
         ))}
       </Row>
@@ -22,3 +16,4 @@ const BookList = ({ books }) => {
 };
 
 export default BookList;
+
